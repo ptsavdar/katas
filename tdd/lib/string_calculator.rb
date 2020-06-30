f9715b08a5df2b8e1400d6d84e798494adbc1630
Panagiotis Tsavdaris <panos@zencargo.com>
2020-06-30 22:44:05 +0100

    StringCalculator kata: Step3

--- tdd/lib/string_calculator.rb
+++ tdd/lib/string_calculator.rb
@@ -1,5 +1,5 @@
 class StringCalculator
   def add(str)
-    str.empty? ? 0 : str.to_i
+    str.empty? ? 0 : str.split(',').reduce(0) { |sum, str_num| sum + str_num.to_i }
   end
 end
--- tdd/test/string_calculator_test.rb
+++ tdd/test/string_calculator_test.rb
@@ -17,4 +17,12 @@ class StringCalculatorTest < Minitest::Test
   def test_another_single_number_string
     assert_equal(50, @string_calculator.add('50'))
   end
+
+  def test_two_numbers_string
+    assert_equal(3, @string_calculator.add('1,2'))
+  end
+
+  def test_another_two_numbers_string
+    assert_equal(56, @string_calculator.add('4,52'))
+  end
 end
